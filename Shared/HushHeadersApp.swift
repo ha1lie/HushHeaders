0d60904204dd3ac94810e23682dd424c81b52921
HallieBot <ryhal.topham@gmail.com>
2021-02-08 14:36:44 -0500

    Initial Commit

---
+++ Shared/HushHeadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HushHeadersApp.swift
+//  Shared
+//
+//  Created by Hallie on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HushHeadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
